018707f5d670556d7b1b53f933bcd9083cde7c5a
claudio Abud <clau@gmail.com>
2024-05-27 10:10:29 -0500

    initial version

---
+++ go.mod
@@ -0,0 +1,8 @@
+module hello
+
+go 1.22.2
+
+require (
+	github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
+	github.com/GoesToEleven/puppy v1.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	fmt.Println("Hello Claudio")
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+}
